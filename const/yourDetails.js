ad79477055f582701b92194ff1f7a9fbd068f7e9
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-01-21 20:45:01 +0000

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,3 +1,4 @@
 import CustomChain from "../const/chain";
-// Replace this with your staking contract address
+export const NETWORK = CustomChain;
+
 export const stakingContractAddress = "0x1DA294910fdf170dC847206825ce408831301fae";
